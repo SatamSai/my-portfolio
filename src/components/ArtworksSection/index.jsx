615f3b6e4df8b72ea73203da716192ea2c73c1f1
SatamSai <sainam7740@gmail.com>
2024-10-14 16:00:14 +0530

    added more artworks

--- src/components/ArtworksSection/index.jsx
+++ src/components/ArtworksSection/index.jsx
@@ -3,8 +3,11 @@ import SectionTemplate from '../SectionTemplate'
 import { Carousel } from 'react-responsive-carousel'
 import { PictureSlider, Text } from './ArtworksSection.styles'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import CatSketch from '../../assets/catsketch.jpg'
-import ThanosSketch from '../../assets/thanos.jpg'
+import Art1 from '../../assets/catsketch.jpg'
+import Art2 from '../../assets/thanos.jpg'
+import Art3 from '../../assets/art3.jpg'
+import Art4 from '../../assets/art4.jpg'
+import Art5 from '../../assets/art5.jpg'
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
 
 const arrowStyles = {
@@ -37,9 +40,11 @@ const ArtWorksSection = () => {
                         )
                     }
                 >
-                    <img src={CatSketch} />
-                    <img src={ThanosSketch} />
-                    <img src={CatSketch} />
+                    <img src={Art1} />
+                    <img src={Art2} />
+                    <img src={Art3} />
+                    <img src={Art4} />
+                    <img src={Art5} />
                 </Carousel>
             </PictureSlider>
         </SectionTemplate>
